dc57679cc16af2f533ff338f99b150156fe3b737
arturhayne <arturhayne@gmail.com>
2023-12-07 22:28:36 -0500

    Decision tree: including num features output
    We checked with different percentile.

--- decision_tree/author-id/dt_author_id.py
+++ decision_tree/author-id/dt_author_id.py
@@ -5,6 +5,7 @@ from time import time
 sys.path.append("/app/tools/")
 from email_preprocess import preprocess
 from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -17,7 +18,8 @@ clf.fit(features_train, labels_train)
 
 pred = clf.predict(features_test)
 
-from sklearn.metrics import accuracy_score
 acc = accuracy_score(pred, labels_test)
+num_features = len(features_train[0])
 
 print("Accuracy:", acc)
+print("Number of features:", num_features)
